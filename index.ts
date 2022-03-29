5569ca94d8e914a88ae414466e7720a181ad3de5
AINASIR <101951230+AINASIR@users.noreply.github.com>
2022-03-29 12:28:57 +0200

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+
+    const personaltoken = req.query.personaltoken;
+
+};
+
+export default httpTrigger;
+
+
